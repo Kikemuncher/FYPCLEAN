b967da70e728acd68c8f33b7aa6093202e1239d4
Kikemuncher <Stapelton@discreet.vip>
2025-03-13 00:31:55 +0200

    Update page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import FeedList from "@/components/feed/FeedList";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen flex justify-center bg-black">
+      <div className="max-w-[500px] w-full">
+        <FeedList />
+      </div>
+    </main>
+  );
+}
