56656c46a8dfa54fceea81ffca160a1a088b7338
Kikemuncher <Stapelton@discreet.vip>
2025-03-13 00:39:42 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} min-h-screen`}>{children}</body>
+      <body className={`${inter.className} min-h-screen bg-black`}>
+        {children}
+      </body>
     </html>
   );
 }
