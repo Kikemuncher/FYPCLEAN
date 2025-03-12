9e947128d87c9e0d9efd7dbced3549e1e601c1e6
Kikemuncher <Stapelton@discreet.vip>
2025-03-13 00:41:08 +0200

    Create video.ts

---
+++ src/types/video.ts
@@ -0,0 +1,12 @@
+export interface VideoData {
+  id: string;
+  username: string;
+  caption: string;
+  song: string;
+  likes: number;
+  comments: number;
+  saves: number;
+  shares: number;
+  videoUrl: string;
+  userAvatar: string;
+}
