b1b77fd9baef7092e79de195d5e8a8edeea7fd35
Kikemuncher <Stapelton@discreet.vip>
2025-03-13 00:38:38 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["placehold.co"], // Add TikTok's image domains when needed
+    domains: ["placehold.co", "i.imgur.com"],
   },
 };
 
