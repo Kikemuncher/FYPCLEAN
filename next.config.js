9a9b12862c8b16d5ad9c6c764dfea4b4b224e488
Kikemuncher <Stapelton@discreet.vip>
2025-03-14 22:00:13 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,6 +5,18 @@ const nextConfig = {
   images: {
     domains: ["placehold.co", "i.imgur.com"],
   },
+  webpack: (config, { isServer }) => {
+    // Fix for the private class fields syntax issue
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+      };
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
