863ef82be0ee065d5106dd9af98328d217b33335
Kikemuncher <Stapelton@discreet.vip>
2025-03-14 22:35:28 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,12 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["placehold.co", "i.imgur.com"],
+    domains: [
+      "placehold.co", 
+      "i.imgur.com", 
+      "randomuser.me", 
+      "assets.mixkit.co"
+    ],
   },
   webpack: (config, { isServer }) => {
     // Fix for the private class fields syntax issue
@@ -16,6 +21,20 @@ const nextConfig = {
       };
     }
     return config;
+  },
+  // Add this to ensure videos can load from various sources
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
+          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' },
+        ],
+      },
+    ];
   },
 };
 
