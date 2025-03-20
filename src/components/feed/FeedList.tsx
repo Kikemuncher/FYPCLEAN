3ee27266934783b175e1a3eb1e98af0ee0bbf1ff
Kikemuncher <Stapelton@discreet.vip>
2025-03-20 23:24:26 +0200

    Update FeedList.tsx

--- src/components/feed/FeedList.tsx
+++ src/components/feed/FeedList.tsx
@@ -1,6 +1,6 @@
 <span className="mr-2">Swipe up for more</span>
           <svg className="h-4 w-4 animate-bounce" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 14l-7 7m0 0l-7-7m7 7V3" />
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 14l7 7m0 0l-7-7m7 7V3" />
           </svg>
         </div>
       )}
