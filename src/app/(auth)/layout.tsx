e1df31b2963776a51be654d22a8cf3f634b355a3
Kikemuncher <Stapelton@discreet.vip>
2025-03-23 21:04:27 +0200

    Update layout.tsx

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,7 +1,11 @@
+"use client";
+
+import { ReactNode } from 'react';
+
 export default function AuthLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: ReactNode;
 }) {
   return (
     <div className="min-h-screen bg-black">
