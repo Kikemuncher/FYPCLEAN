d77300ea7acdf341a32a81910a2f3a25ac5f7383
Kikemuncher <Stapelton@discreet.vip>
2025-04-02 01:16:07 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import { useState, useEffect } from "react";
 import dynamic from "next/dynamic";
 import MainLayout from "@/components/layout/MainLayout";
