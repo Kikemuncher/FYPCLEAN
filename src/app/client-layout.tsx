323e765088a0102c95ab3bf414c4c75352715a52
Kikemuncher <Stapelton@discreet.vip>
2025-04-01 18:05:37 +0200

    Create client-layout.tsx´

---
+++ src/app/client-layout.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+// src/app/client-layout.tsx
+import { ReactNode } from 'react';
+import { AuthProvider } from '@/context/AuthContext';
+
+export default function ClientLayout({ children }: { children: ReactNode }) {
+  return (
+    <AuthProvider>
+      {children}
+    </AuthProvider>
+  );
+}
