e89abbffa14ddbe37bf4bc8896d25d3d1681ea2d
Kikemuncher <Stapelton@discreet.vip>
2025-04-04 21:02:58 +0200

    Update page.tsx

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -4,7 +4,7 @@
 import { useState } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
-import { useAuth } from '@/context/AuthContext';
+import { useAuth } from '@/hooks/useAuth';
 
 export default function SignupPage() {
   const [email, setEmail] = useState('');
